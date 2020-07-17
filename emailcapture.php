244fc3a83a64963578f75b20925685df554e2ad2
Trillzdboy <chukwudubem7@gmail.com>
2020-07-17 07:51:32 +0100

    update capture

--- emailcapture.php
+++ emailcapture.php
@@ -1,24 +1,32 @@
-<? php
-$conn = mysqli_connect('localhost', 'root', '', 'emailcapture');
+<?php
 
+// get value of email on submit
+  $email = $_POST['email'];
 
-if(isset($_POST['submit'])){
-	$email = $_POST['email'];
-	$email = mysqli_real_escape_string($conn, $email)
-	;
-	
-	$query = "INSERT INTO foetuslist (email)
-	VALUES ('$email')";
-	$result = mysqli_query($conn,$query);
-	if($result){
+  $conn = new mysqli('localhost','root','','emailcapture');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into foetuslist(email) values(?)");
+		$stmt->bind_param("s", $email);
+		$execval = $stmt->execute();
 		echo "<script>
 		alert ('successfully subscribed');
-		window.location.pathname = 'phph/index.html';
+		window.location.pathname = 'index.html';
 		</script>";
-	} else{
-		echo "<script>
-		alert ('unsuccessful');
-		window.location.pathname = 'phph/index.html';
-		</script>";
+		$stmt->close();
+		$conn->close();
 	}
-}
+
+	if (empty($_POST["email"])) {
+		$emailErr = "Email is required";
+	  } else {
+		$email = ($_POST["email"]);
+		// check if e-mail address is well-formed
+		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		  $emailErr = "Invalid email format";
+		}
+	  }
+
+?>
